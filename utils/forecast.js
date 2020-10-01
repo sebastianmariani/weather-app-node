d46fe71fda67e2526b711dbb52874139aff94f3d
User <sebastian.mariani.94@gmail.com>
2020-10-01 17:06:11 +0100

    refactor shorthand es6

--- utils/forecast.js
+++ utils/forecast.js
@@ -2,13 +2,13 @@ const request = require('request')
 
 const forecast = (latitude, longitude, callback) => {
     const url = 'http://api.weatherstack.com/current?access_key=7180aac380a211a6f59853956ef3fa0d&query=' + latitude + ',' + longitude + ''
-    request({ url: url, json: true }, (error, response) => {
+    request({ url, json: true }, (error, { body }) => {
         if (error) {
             callback('Unable to connect to weather service!', undefined)
-        } else if (response.body.error) {
+        } else if (body.error) {
             callback(`Unable to find location.`, undefined)
         } else {
-            callback(undefined, `${response.body.current.weather_descriptions[0]}. It is currently ${response.body.current.temperature} degrees out. It feels like there are ${response.body.current.feelslike} degrees.`)
+            callback(undefined, `${body.current.weather_descriptions[0]}. It is currently ${body.current.temperature} degrees out. It feels like there are ${body.current.feelslike} degrees.`)
         }
     }) 
 }
